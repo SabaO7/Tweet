42c825b8bee5f6103799fd30e17cab17eeab6328
Saba Oji <sabaoji@gmail.com>
2023-06-27 18:34:13 -0400

    Tweeter for Code Review

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#tweet-text").on("keyup", function (e) {
+    //Get current count of the string in the text area
+    const currCount = $(this).val().length;
+    //Get the value of the counter
+    const counter = $(this)
+      .parent()
+      .parent()
+      .children(".tweet-additions")
+      .children(".counter");
+    const count = 140 - currCount;
+    //Update the counter
+    counter.text(count);
+    if (count < 0) {
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "#545149");
+    }
+  });
+});
